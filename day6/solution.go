5f2f55d3921dc7b9df561e3d848c79679e654bdb
Michal Grzybek <michal.grzybek@dynatrace.com>
2020-12-06 15:47:38 +0100

    day 6.2

--- day6/solution.go
+++ day6/solution.go
@@ -7,17 +7,17 @@ import (
 	"strings"
 )
 
+type RuneSetT = map[rune]struct{}
 
-
-func loadData(reader io.Reader) []map[rune]struct{} {
+func loadData1(reader io.Reader) []RuneSetT {
 	scanner := bufio.NewScanner(reader)
 
-	groups := make([]map[rune]struct{}, 0)
+	groups := make([]RuneSetT, 0)
 	firstLoop := true
 	for scanner.Scan() {
 		text := scanner.Text()
 		if firstLoop || strings.EqualFold(text, "") {
-			groups = append(groups, make(map[rune]struct{}))
+			groups = append(groups, make(RuneSetT))
 			firstLoop = false
 		}
 
@@ -33,9 +33,9 @@ func loadData(reader io.Reader) []map[rune]struct{} {
 
 func Solve1() int {
 	f, _ := os.Open("day6/input")
-	groups := loadData(f)
+	groups := loadData1(f)
 
-	sum :=0
+	sum := 0
 
 	for _, group := range groups {
 		sum += len(group)
@@ -43,6 +43,57 @@ func Solve1() int {
 	return sum
 }
 
+func processData2(reader io.Reader) []RuneSetT {
+	scanner := bufio.NewScanner(reader)
+
+	intersections := make([]RuneSetT, 0)
+
+	inGroupResponses := make([]RuneSetT, 0)
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		if text == "" {
+			intersections = append(intersections, calculateIntersection(inGroupResponses))
+
+			// reset state
+			inGroupResponses = make([]RuneSetT, 0)
+			continue
+		}
+
+		m := make(RuneSetT)
+		for _, letter := range text {
+			m[letter] = struct{}{}
+		}
+
+		inGroupResponses = append(inGroupResponses, m)
+	}
+
+	return append(intersections, calculateIntersection(inGroupResponses))
+}
+
+
+func calculateIntersection(responses []RuneSetT) RuneSetT {
+	// end of group, run intersection on lines in group
+	intersection := responses[0]
+	for i := 1; i < len(responses); i++ {
+		for r, _ := range intersection {
+			_, exists := responses[i][r]
+			if !exists {
+				delete(intersection, r)
+			}
+		}
+	}
+	return intersection
+}
+
 func Solve2() int {
-	return -1
+	f, _ := os.Open("day6/input")
+	intersections := processData2(f)
+
+	sum := 0
+	for _, intersection := range intersections {
+		sum += len(intersection)
+	}
+
+	return sum
 }
